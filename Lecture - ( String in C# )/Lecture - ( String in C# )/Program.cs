7e08c166bb8d1f23ffd265bf32a8cfb6b1a79081
codexhub01 <mayankpal4104@gmail.com>
2026-05-17 13:37:53 +0530

    Lecture - ( String in C# )

---
+++ Lecture - ( String in C# )/Lecture - ( String in C# )/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ABC
+{
+    class A
+    {
+        public static void Main(String[] args)
+        {
+            string s = "mayank";
+            
+        }
+    }
+}
+
+/*
+    
+String :-
+-> A sequence of characters
+-> It's not primitive types but a refrence type class from System.String
+-> string is a alisa of System.String
+-> Strings are immutable which means it can not be modified after creation
+-> To modify in string everytime new object creates
+-> StringBuilder which is mutable means every time modifies same object
+
+
+
+
+ */
