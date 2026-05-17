ad7fd6f18aad2263b3ff545d9c97524c67d1ad66
codexhub01 <mayankpal4104@gmail.com>
2026-05-17 12:32:31 +0530

    Lecture - ( CallByValueAndReference )

---
+++ Lecture - ( CallByValueAndReference )/Lecture - ( CallByValueAndReference )/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ABC
+{
+    class B
+    {
+        public void passed_by_val(int val)
+        {
+            Console.WriteLine(val);
+        }
+
+        public void passed_by_ref(ref int val)
+        {
+            val += 100;
+            Console.WriteLine(val);
+        }
+
+        //public void passed_by_out(out int a)
+        //{
+        //    //a += 100;
+        //    Console.WriteLine(val);
+        //}
+    }
+
+    class A
+    {
+        public static void Main(string[] args)
+        {
+            B b = new B();
+            b.passed_by_val(1);
+
+            //passed  by ref
+            int x = 12;
+            b.passed_by_ref(ref x);
+            Console.WriteLine(x);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*
+    
+When we pass variables into methods , C# pass them in mainly two ways
+-> Call by value ( copy passed )
+-> Call by refrence ( actual variable refrence passed )
+
+Call By value :-
+-> copy of variable is passed to method
+-> original variable does not change because method works on copied value
+-> Must intialize before pass
+-> It usually stored in stack
+-> Stack copy created
+
+Call By Reference :-
+-> Actual variable reference/address passed into method
+-> Affect original variable
+-> use using ref keyword
+-> Same stack location refrenced
+
+Out Keyword:-
+-> It also use refrence passing
+-> No intialization needed
+
+
+ */
