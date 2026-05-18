dcec3b7cc0662b4e5babdc5e2724a6efa9024397
codexhub01 <mayankpal4104@gmail.com>
2026-05-18 20:25:15 +0530

    Interface in C#

---
+++ Interface in C#/Interface in C#/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace ABC
+{
+    interface I1
+    {
+        public void M1()
+        {
+            Console.WriteLine("Method inside interface");
+        }
+        
+    }
+
+    class P1 : I1
+    {
+
+        public void M1()
+        {
+            
+            Console.WriteLine("Method inside class");
+        }
+    }
+
+    class X
+    {
+        public static void Main(String[] args)
+        {
+           P1 p = new P1();
+            p.M1();
+            
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*  
+ 
+ Interface :-
+-> Its a kind of contract thatd defines what a class must implement
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
