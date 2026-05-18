04c5b760816a621aa3fb2886f722916cc9c8ede3
codexhub01 <mayankpal4104@gmail.com>
2026-05-18 16:55:28 +0530

    Stack and Heap Memory in .NET

---
+++ Stack and Heap Memory in .NET/Stack and Heap Memory in .NET/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ABC
+{
+    class A
+    {
+        public static void Main(String[] args)
+        {
+
+        }
+    }
+}
+/*
+ 
+Stack :- 
+-> fast memory used for method execution & local variables
+-> Stores temporary execution data
+-> LIFO
+-> Automatically clean itself
+-> Stores directly
+
+Heap :-
+-> memory used for dynamic object allocation
+-> It stores actual object
+-> Its memory managed by garbage collector
+-> remains until garbage collector removes them
+-> Object stored in heap
+
+ */
