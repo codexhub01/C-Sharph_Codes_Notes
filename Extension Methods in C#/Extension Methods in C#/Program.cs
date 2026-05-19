2c9fe4223cf54d91404623f9003264f96e66ad7c
codexhub01 <mayankpal4104@gmail.com>
2026-05-19 10:02:15 +0530

    Extension Methods in C# ( Pending )

---
+++ Extension Methods in C#/Extension Methods in C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ABC
+{
+    class X
+    {
+        public static void main(String[] args)
+        {
+
+        }
+    }
+}
+
+/*  
+ Extension methods :-
+-> adding new methods to existing class without modifying source code
+-> these methods looks lke existing class already cintain that method , wevn though we never change original class
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
