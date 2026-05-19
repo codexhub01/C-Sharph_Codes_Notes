5ac10e18898f59189cf78bb1a039a2c9acce3255
codexhub01 <mayankpal4104@gmail.com>
2026-05-19 18:13:20 +0530

    Anonymous Method in C#

---
+++ Anonymous Method in C#/Anonymous Method in C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ABC
+{
+
+    delegate void my_delegate();
+    class X
+    {
+        public static void main(String[] args)
+        {
+            //my_delegate d = new delegate()
+            //{
+            //    Console.WriteLine("hj");
+            //};
+            //d();
+        }
+    }
+}
+/*  
+ Anonymous method :-
+-> A method without name
+-> these are mainly used with delegates
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
