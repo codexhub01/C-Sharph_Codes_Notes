307b28e725de53d351fff151217e19ee91c38d33
codexhub01 <mayankpal4104@gmail.com>
2026-05-19 10:19:32 +0530

    Exception Handling in C#

---
+++ Exception Handling in C#/Exception Handling in C#/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace ABC
+{
+    class X
+    {
+        public static void main(String[] args)
+        {
+            int x = 10;
+            int y = 0;
+            int res = x / y;
+
+            //try-catch
+            try
+            {
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Exception handled");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide by zero exception");
+            }
+        }
+    }
+}
+
+/*  
+ 
+ Exception :-
+-> run time error that interups normal program execution
+
+Exception handling keywords :-
+-> try
+-> catch
+-> finally :- this block executes always whether the exceptions occurs orot
+-> throw
+
+-> multiple catch blocks can be there
+-> specific type of exception must comes first before ( Exception )
+ 
+
+
+
+
+
+
+
+
+ 
+ 
+ */
