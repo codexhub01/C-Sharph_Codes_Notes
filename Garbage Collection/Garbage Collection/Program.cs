0598afa94b943323135a52f99446fa8b66dd2582
codexhub01 <mayankpal4104@gmail.com>
2026-05-31 13:33:20 +0530

    Garbage Collection

---
+++ Garbage Collection/Garbage Collection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ABC
+{
+    class X
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+/* 
+ 
+Garbage Collector (GC) is a CLR component that automatically identifies and removes objects that are no longer being used.
+
+ 
+ */
